8561cfcebd5fbc57ec6b7487b3796f15ef8a1c2a
kotaro_k <kotaro.k13@gmail.com>
2019-02-05 17:40:43 +0900

    add each directive and data compoent

--- app/Http/Controllers/ExtendController.php
+++ app/Http/Controllers/ExtendController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class ExtendController extends Controller
 {
     public function index(){
-        return view('extend.index');
+        $data = [
+            ['name'=>'山田たろう', 'mail'=>'taro@yamada'],
+            ['name'=>'田中はなこ', 'mail'=>'hanako@tanaka'],
+            ['name'=>'鈴木さちこ', 'mail'=>'sachiko@suzuki']
+        ];
+        return view('extend.index',['data'=>$data]);
     }
 }
--- resources/views/extend/index.blade.php
+++ resources/views/extend/index.blade.php
@@ -10,6 +10,22 @@
 @section('content')
     <p>ここが本文のコンテンツです。</p>
     <p>必要なだけ記述できます。</p>
+
+    @each('components.item', $data, 'item')
+
+    <!-- @include('components.message', ['msg_title'=>'OK',
+    'msg_content'=>'サブビューです。']) -->
+
+    <!-- @component('components.message')
+        @slot('msg_title')
+            CAUTION!
+        @endslot
+
+        @slot('msg_content')
+            これはメッセージの表示です。
+        @endslot
+    @endcomponent -->
+
 @endsection
 
 @section('footer')
