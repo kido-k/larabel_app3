fb225783507102e29786d39c939d6d9de924cd1d
kotaro_k <kotaro.k13@gmail.com>
2019-02-05 17:07:03 +0900

    add layout component

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -19,3 +8,5 @@ Route::get('hello','HelloController@index');
 Route::post('hello','HelloController@post');
 
 Route::get('for','ForController@index');
+
+Route::get('extend','ExtendController@index');
