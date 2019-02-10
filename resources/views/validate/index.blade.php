900c93ffe94fcdf2a4611eb76579d4820f3ae0a0
kotaro_k <kotaro.k13@gmail.com>
2019-02-10 22:39:56 +0900

    update error msg logic

--- resources/views/validate/index.blade.php
+++ resources/views/validate/index.blade.php
@@ -10,29 +10,49 @@
 @section('content')
 <p>{{$msg}}</p>
 @if(count($errors)>0)
-<div>
+<p>入力に問題があります。再入力してください。</p>
+{{-- <div>
     <ul>
         @foreach ($errors->all() as $error)
         <li>{{$error}}</li>
         @endforeach
     </ul>
-</div>
+</div> --}}
 @endif
 <table>
     <form action="/validate" method="post">
         {{csrf_field()}}
+        @if($errors->has('name'))
+        <tr>
+            <th>ERROR</th>
+            <td>{{$errors->first('name')}}</td>
+        </tr>
+        @endif
         <tr>
             <th>name: </th>
-            <td><input type="text" name="name"></td>
+            <td><input type="text" name="name" value="{{old('name')}}"></td>
         </tr>
+        @if($errors->has('mail'))
+        <tr>
+            <th>ERROR</th>
+            <td>{{$errors->first('mail')}}</td>
+        </tr>
+        @endif
         <tr>
             <th>mail: </th>
             <td><input type="text" name="mail"></td>
         </tr>
+        @if($errors->has('age'))
+        <tr>
+            <th>ERROR</th>
+            <td>{{$errors->first('age')}}</td>
+        </tr>
+        @endif
         <tr>
             <th>age: </th>
             <td><input type="text" name="age"></td>
         </tr>
+
         <tr>
             <th></th>
             <td><input type="submit" value="send"></td>
