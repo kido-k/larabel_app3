f9b610dd4e526683e1baf46d87569a0004bc3083
kotaro_k <kotaro.k13@gmail.com>
2019-02-12 08:47:17 +0900

    add to use placeholder

--- app/Http/Controllers/DBController.php
+++ app/Http/Controllers/DBController.php
@@ -7,12 +7,24 @@ use Illuminate\Support\facades\DB;
 
 class DBController extends Controller
 {
-    public function index(Request $request){
-        $items = DB::select('select * from people');
+    public function index(Request $request)
+    {
+        // first
+        // $items = DB::select('select * from people');
+        // return view('db.index', ['items' => $items]);
+
+        // second
+        if (isset($request->id)) {
+            $param = ['id' => $request->id];
+            $items = DB::select('select * from people where id = :id', $param);
+        } else {
+            $items=DB::select('select * from people');
+        }
         return view('db.index', ['items' => $items]);
     }
 
-    public function post(Request $request){
-        return view('db.index', ['msg'=>$request->msg]);
+    public function post(Request $request)
+    {
+        return view('db.index', ['msg' => $request->msg]);
     }
 }
