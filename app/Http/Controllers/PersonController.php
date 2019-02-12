06723ea9d47650c16a223a5fb6f54045a0b56376
kotaro_k <kotaro.k13@gmail.com>
2019-02-13 02:30:18 +0900

    add scope function in person class

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -2,29 +2,45 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Person;
+use Illuminate\Http\Request;
 
 class PersonController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $items = Person::all();
         return view('person.index', ['items' => $items]);
     }
 
-    public function find(Request $request){
-        return view('person.find', ['input' => '' ]);
+    public function find(Request $request)
+    {
+        return view('person.find', ['input' => '']);
     }
 
-    public function search(Request $request){
+    public function search(Request $request)
+    {
         // first
         // $item = Person::find($request->input);
         // $param = ['input'=> $request->input, 'item'=> $item];
         // return view('person.find', $param);
 
         // second
-        $item = Person::where('name', $request->input)->first();
-        $param = ['input'=> $request->input, 'item'=> $item];
+        // $item = Person::where('name', $request->input)->first();
+        // $param = ['input'=> $request->input, 'item'=> $item];
+        // return view('person.find', $param);
+
+        // third
+        // $item = Person::nameEqual($request->input)->first();
+        // $param = ['input' => $request->input, 'item' => $item];
+        // return view('person.find', $param);
+
+        // fouth
+        $min = $request->input * 1;
+        $max = $min + 10;
+        $item = Person::ageGreaterThan($min)
+            ->ageLessThan($max)->first();
+        $param = ['input' => $request->input, 'item' => $item];
         return view('person.find', $param);
     }
 }
