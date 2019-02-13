5800f0fe08aacc1ca1b55034539780fb212b7356
kotaro_k <kotaro.k13@gmail.com>
2019-02-13 16:51:20 +0900

    add has many method

--- app/Person.php
+++ app/Person.php
@@ -42,7 +42,11 @@ class Person extends Model
         'age' > 'integer|min:0|max:150',
     ]);
 
-    public function board(){
-        return $this->hasOne('App\Board');
+    // public function board(){
+    //     return $this->hasOne('App\Board');
+    // }
+
+    public function boards(){
+        return $this->hasMany('App\Board');
     }
 }
